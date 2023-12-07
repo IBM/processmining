852b95a7c83725b49b2f629b0d3862d15a6d1f85
Patrick-Megard <patrick.megard@fr.ibm.com>
2023-12-07 13:17:18 +0100

    countWorkingDays custom metric fixes

--- Custom Metrics/CaseWorkingDays.js
+++ Custom Metrics/CaseWorkingDays.js
@@ -12,13 +12,13 @@ function countWeekendDays(d0, d1, ndays){
 }
 
 function excludeWeekendFromVacations(vacations){
-  var vacationWorkingDates = [];
-  for (var i=0; i<vacations.length; i++){
-    var d = new Date(vacations[i]);
-    if (d.getDay()!=0 && d.getDay() != 6) // not a weekend
-       vacationWorkingDates.push(new Date(vacations[i]));
-  }
-  return vacationWorkingDates;
+  var vacationsWithoutWeekends = [];
+  for (var i=0; i<vacations.length;i++){
+    
+    if (vacations[i].getDay()>0 && vacations[i].getDay()<6)
+    	vacationsWithoutWeekends.push(vacations[i]);
+  } 
+  return vacationsWithoutWeekends;
 }
 
 function countWorkingDays(startTime, endTime, vacations){
@@ -27,26 +27,37 @@ function countWorkingDays(startTime, endTime, vacations){
   var endDate = new Date(endTime);
   if (startDate.getDate() == endDate.getDate())
     return 1; // case completed within a day
-  startDate.setHours(0,0);
-  endDate.setHours(0,0)
+  
+  startDate.setUTCHours(0,0,0,0);
+  endDate.setUTCHours(0,0,0,0);
   var ndays = (endDate - startDate)/millisecondsPerDay + 1;
+  
   // remove weekends
   ndays -= countWeekendDays(startDate, endDate, ndays);
+  
   // remove vacation days that are not saturday or sunday
   var vacationsWithoutWeekends = excludeWeekendFromVacations(vacations);
-  for (var i=0; i < vacationsWithoutWeekends.length; i++){
-    if (startDate < vacationsWithoutWeekends[i] && endDate > vacationsWithoutWeekends[i])
+  
+  for (var j=0; j < vacationsWithoutWeekends.length; j++){
+    if (startDate < vacationsWithoutWeekends[j] && endDate > vacationsWithoutWeekends[j])
       ndays--;
   }
+  
+  
   return ndays;
 }
 
-var vacationDays = ["2023-01-01","2023-05-01", "2023-05-08", "2023-07-14", "2023-08-15","2023-11-1", "2023-11-11","2023-12-25"];
+
 
 // This is for multi-event logs where the latest activity of a case is not always the last one.
 // It works also for flat processes. 
 var customMetric = {
-	caseMetric: function(aCase) {
+	caseMetric: function(aCase){
+      // Ideally we would just set the dates as strings like "2023-1-1", but the JS server does not allow
+      //the creation of dates through string.    
+      var vacationDays = [new Date(2023,1,1), new Date(2023,5,1), new Date(2023,7,14), new Date(2023,8,15),
+                         new Date(2023,11,1), new Date(2023,11,11), new Date(2023,12,25)];
+                         
       var startTime = Number.MAX_VALUE;
       var endTime = 0;
       for(var k = 0 ; k < aCase.size(); k++) {
@@ -73,7 +84,34 @@ let startTime = "2023-12-22T10:00:00";
 let endTime = "2023-12-26T20:01:00";
 let millisecondsPerDay = 24 * 60 * 60 * 1000;
 
+startTime = "2018-11-02T15:03:56";
+console.log(new Date(startTime));
+startTimeMs = 1541171036000; //1541171036000
+console.log(new Date(startTimeMs));
+
+endTime = "2018-11-05T16:36:39";
+endTimeMs = 1541435799000; //1541435799000
+console.log(new Date(endTime));
+console.log(new Date(endTimeMs));
+
+d0 = new Date(startTime);
+d1 = new Date(endTime);
+console.log(d1-d0);
+
+d0.setHours(0,0,0,0);// 1541404800000
+console.log(d0);
+console.log(d0.getTime()); //1541142000000
+console.log("diff" + ((d0.getTime() - 1541142000000)/(60*60*1000)))
+
+
+d1.setHours(0,0,0,0);// 1541142000000
+console.log(d1.getTime());
+console.log(d1-d0);
+console.log((d1-d0)/millisecondsPerDay + 1)
+
 var vacationsWithoutWeekends = excludeWeekendFromVacations(vacationDays);
 console.log(vacationsWithoutWeekends);
+console.log(new Date(startTime));
+console.log(new Date(endTime));
 console.log(countWorkingDays(startTime, endTime, vacationsWithoutWeekends));
 
