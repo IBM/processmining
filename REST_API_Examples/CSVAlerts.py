29538c8f73ac7cd60d36f308b9c9b028c4984002
Patrick-Megard <patrick.megard@fr.ibm.com>
2024-02-19 11:24:42 +0100

    IPM connector class added

--- REST_API_Examples/CSVAlerts.py
+++ REST_API_Examples/CSVAlerts.py
@@ -18,13 +18,23 @@ def join_and_cleanup(df1, df2, cols, suffix, suffix_for):
             kept_cols.append(col)
     return df[kept_cols]
 
-class widgetAlerts():
-    def __init__(self, config): # config is a json with the widget properties
+class processMiningConnector():
+    def __init__(self, config):
         self.config = config
-        self.dashboardName = config['dashboard_name']
-        self.widgetId = config['widget_id']
+
+
+class widgetAlerts():
+    def __init__(self, connector, dashboardName, widgetId, matchingColumnIndexes): # config is a json with the widget properties
+        self.connector = connector
+        self.dashboardName = dashboardName
+        self.widgetId = widgetId
+        self.matchingColumnIndexes = matchingColumnIndexes # replaced when the widget columns are known
+        if (self.matchingColumnIndexes == 0 or self.matchingColumnIndexes == ''):
+            self.matchingColumnIndexes = [0]
         self.logFileName = 'alert_log_' + self.dashboardName + '_' + self.widgetId +'.csv'
         self.summaryFileName = 'alert_summary_' + self.dashboardName + '_' + self.widgetId +'.csv'
+        connector.config['dashboard_name'] = dashboardName
+        connector.config['widget_id'] = widgetId
 
 
     def loadLog(self):
@@ -44,23 +54,23 @@ class widgetAlerts():
             self.summary_df = pd.DataFrame()
 
     def loadWidgetData(self):
-        IPM.ws_post_sign(self.config)
-        res = IPM.ws_get_widget_values(self.config)
+        IPM.ws_post_sign(self.connector.config)
+        res = IPM.ws_get_widget_values(self.connector.config)
         if (res['status_code'] == 200):
             self.widget_df = pd.DataFrame(res['data'])
+            self.setMatchingColumns() # matching columns: indexes replaced with column names
+
         else: 
             print('ERROR: Dashboard or Widget not found')
             self.widget_df = pd.DataFrame()
 
-    def setMatchingCols(self, cols):
+    def setMatchingColumns(self):
         # Get the widget columns used to match the alerts
         # Do the merge with the columns mentionned in the configuration as an array
-        if cols == 0 or cols == '' : self.matchingColumns = self.widget_df.columns[0]
-        else:
-            self.matchingColumns = []
-            for i in range(len(self.widget_df.columns)):
-                if (i in cols):
-                    self.matchingColumns.append(self.widget_df.columns[i])
+        self.matchingColumns = []
+        for i in range(len(self.widget_df.columns)):
+            if (i in self.matchingColumnIndexes):
+                self.matchingColumns.append(self.widget_df.columns[i])
         print("Matching columns used for existing alerts: %s" % self.matchingColumns)
     
     def updateAlertsFirstTime(self):
@@ -90,7 +100,6 @@ class widgetAlerts():
         self.loadLog()
         self.loadSummary()
         self.loadWidgetData()
-        self.setMatchingCols(self.config['alert_matching_columns'])
 
         if len(self.log_df) == 0: # No log yet
             self.updateAlertsFirstTime()
@@ -217,31 +226,28 @@ class widgetAlerts():
         return self.summary_df
     
     def setMatchingColumnIndexes(self, cols):
-        self.config['alert_matching_columns'] = cols
+        self.matchingColumnIndexes = cols
 
 def main(argv):
     if (len(argv)==2):
         try:
             with open(argv[1], 'r') as file:
-                myconfig = json.load(file)
-                print('Loading config file')
-                print(myconfig)
+                connectorConfig = json.load(file)
+                print('Loading connector configuration file')
+                print(connectorConfig)
         except:
-            print('Error config file not found') 
+            print('Error connector configuration file not found') 
     else:
-        myconfig = {
+        connectorConfig = {
             "url":"https://ProcessMining.com",
             "user_id": "john.smith",
             "api_key":"8a5kga87eqvd1180",
             "project_key": "procure-to-pay",
             "org_key": "",
-            "dashboard_name": "Alerts Dashboard",
-            "widget_id": "alerts-widget-1",
-            "alert_matching_columns": [0,1,2,3], # Index of columns that are used to match current/new alerts
-
         }
-    # Constructor
-    alerts1 = widgetAlerts(myconfig)
+    # Constructors
+    connector = processMiningConnector(connectorConfig)
+    alerts1 = widgetAlerts(connector, 'alerts', 'invoices-withholding-tax', [0,1,2,3])
     # Optionnaly, the default filenames can be changed like this:
     alerts1.setLogFilename('mylog.csv')
     alerts1.setSummaryFilename('mysummary.csv')
@@ -250,6 +256,12 @@ def main(argv):
     alerts1.updateAlertsAndSave() # Load files, get widget values, match with log, update log and summary, save files
     print(alerts1.getLogs().head())
     print(alerts1.getSummary().head())
+
+    alerts2 = widgetAlerts(connector, 'alerts', 'invoices-blocked-account', [0,1,2,3])
+    alerts2.updateAlertsAndSave()
+    print(alerts2.getLogs().head())
+    print(alerts2.getSummary().head())
+
 
 if __name__ == "__main__":
     main(sys.argv)
