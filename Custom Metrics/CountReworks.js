c524fe295c9157ff9ec86f0014a630383eca680d
Patrick-Megard <patrick.megard@fr.ibm.com>
2024-04-12 10:33:09 +0200

    custom metrics reworks and loops

--- Custom Metrics/CountReworks.js
+++ Custom Metrics/CountReworks.js
@@ -1,6 +1,6 @@
 var customMetric = {
     caseMetric: function (aCase) {
-        var nbr_loops = 0;
+        var nbr_reworks = 0;
         var activities = [];
         var counters = [];
         for (var k = 0; k < aCase.size(); k++) {
@@ -8,14 +8,14 @@ var customMetric = {
             var index = activities.indexOf(activity);
             if (index < 0) { 
                 activities.push(activity);
-                counters.push(1);
+                counters.push(0);
             }
             else { 
                 counters[index] += 1;
             }
         }
         for (var i = 0; i < counters.length; i++)
-            nbr_loops += counters[i];
-        return nbr_loops;
+            nbr_reworks += counters[i];
+        return nbr_reworks;
     }
 };
